b657b78e9c230a7f00d5a270d3bb6be9a16c15f8
ATSUGI COFFEE <atsugicoffee@gmail.com>
2025-04-16 15:15:15 +0900

    Delete pages directory

--- pages/index.js
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div style={{ padding: "2rem", fontFamily: "sans-serif" }}>
-      <h1>Welcome to BlendMaster ☕</h1>
-      <p>This is your starting point. Let's create your first blend!</p>
-    </div>
-  );
-}
